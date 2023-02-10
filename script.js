9c1f059dbe09750a6dca5d6ed94e021f5be212e4
IchirouTakashima <ichiroutakashimakun@gmail.com>
2023-02-10 16:31:59 +0800

    new updates

--- script.js
+++ script.js
@@ -5,12 +5,18 @@ const errorsList = document.getElementById("errors-list")
 const errorIcon = document.getElementById("icon-error")
 const inputBorder = document.getElementById("input-container")
 const header = document.getElementById("header-content")
+const text = document.getElementById("text-content")
+const submit = document.getElementById("submit")
 
 function showError(errorMessages) {
   errorMessages.forEach((errorMSG) => {
     const li = document.createElement("li")
     li.innerText = errorMSG
     errorContainer.appendChild(li)
+    // if (window.innerWidth > 2000) {
+    //   li.style.fontSize = "2.5em"
+    //   form.style.margin = "20px 20px"
+    // }
   })
 }
 
@@ -44,11 +50,30 @@ form.addEventListener("submit", (e) => {
   checkEmail(email)
 })
 
-window.addEventListener("resize", function () {
-  // document.body.style.fontSize = window.innerWidth / 100 + "px"
-  let fontSize = window.innerWidth / 20 + "px"
-  if (parseFloat(fontSize) < 40) {
-    fontSize = "45px"
-  }
-  header.style.fontSize = fontSize
-})
+// window.addEventListener("resize", function () {
+//   // document.body.style.fontSize = window.innerWidth / 100 + "px"
+//   let fontSize = window.innerWidth / 20 + "px"
+//   if (parseFloat(fontSize) < 40) {
+//     fontSize = "50px"
+//   }
+
+//   if (window.innerWidth > 768) {
+//     text.style.paddingLeft = "20px"
+//     text.style.maxWidth = "550px"
+//     inputBorder.style.maxWidth = "85%"
+//     form.style.paddingLeft = "20px"
+//     header.style.paddingLeft = "20px"
+//   }
+
+//   if (window.innerWidth > 2000) {
+//     header.style.fontSize = "10em"
+//     text.style.fontSize = "3em"
+//     email.style.fontSize = "3.5em"
+//     text.style.maxWidth = "90%"
+//     submit.style.padding = "30px 60px"
+//     submit.style.borderRadius = "50px"
+//     inputBorder.style.borderRadius = "50px"
+//   }
+
+//   header.style.fontSize = fontSize
+// })
